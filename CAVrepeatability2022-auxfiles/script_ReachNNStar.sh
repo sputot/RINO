75293e5823803cc94be582d0f1e0ecdbede99461
Eric Goubault <goubault@lix.polytechnique.fr>
2022-04-27 09:13:57 +0200

    new path

--- CAVrepeatability2022-auxfiles/script_ReachNNStar.sh
+++ CAVrepeatability2022-auxfiles/script_ReachNNStar.sh
@@ -1,12 +1,14 @@
-/CAV21_repeatability_package/ReachNNStar/ReachNN/run_ex1_tanh.sh 
-/CAV21_repeatability_package/ReachNNStar/ReachNN/run_ex1_sig.sh 
-/CAV21_repeatability_package/ReachNNStar/ReachNN/run_ex2_tanh.sh 
-/CAV21_repeatability_package/ReachNNStar/ReachNN/run_ex2_sig.sh 
-/CAV21_repeatability_package/ReachNNStar/ReachNN/run_ex3_tanh.sh 
-/CAV21_repeatability_package/ReachNNStar/ReachNN/run_ex3_sig.sh
-/CAV21_repeatability_package/ReachNNStar/ReachNN/run_ex4_tanh.sh 
-/CAV21_repeatability_package/ReachNNStar/ReachNN/run_ex4_sig.sh
-/CAV21_repeatability_package/ReachNNStar/ReachNN/run_ex5_tanh.sh
-/CAV21_repeatability_package/ReachNNStar/ReachNN/run_ex5_sig.sh
-/CAV21_repeatability_package/ReachNNStar/ReachNN/run_tora_tanh.sh
-/CAV21_repeatability_package/ReachNNStar/ReachNN/run_tora_sig.sh
+cd /CAV21_repeatability_package/ReachNNStar/ReachNN
+
+./run_ex1_tanh.sh 
+./run_ex1_sig.sh 
+./run_ex2_tanh.sh 
+./run_ex2_sig.sh 
+./run_ex3_tanh.sh 
+./run_ex3_sig.sh
+./run_ex4_tanh.sh 
+./run_ex4_sig.sh
+./run_ex5_tanh.sh
+./run_ex5_sig.sh
+./run_tora_tanh.sh
+./run_tora_sig.sh
